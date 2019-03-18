d39ed2e563a3185a7133b38459ffc757517e854d
Vincent <enchanter111@gmail.com>
2019-03-18 14:09:42 +0100

    1st commit

---
+++ src/main/java/com/sr03p1/sr03_p1/Sr03P1Application.java
@@ -0,0 +1,13 @@
+package com.sr03p1.sr03_p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr03P1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sr03P1Application.class, args);
+    }
+
+}
